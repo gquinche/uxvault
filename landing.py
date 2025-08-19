78978fa7a8b8bff9c8603272936d2dae3019894b
Gabriel Quinche <gabrielquinche@hotmail.com>
2025-08-19 01:11:43 +0000

    remove css hack

--- landing.py
+++ landing.py
@@ -11,26 +11,17 @@ st.set_page_config(
     page_icon=SMALL_LOGO,
     layout="centered",
     initial_sidebar_state="expanded",)
-st.logo(image = BIG_LOGO,icon_image=BIG_LOGO,size="large")
 
-st.html("""
-        <style>
+# detect 
+st.logo(image = BIG_LOGO,size="large")
 
 
-.rc-overflow {
-    display: flex;
-    justify-content: flex-end;
-    margin-right: -7rem;
-}
-        </style>
-""")
-
-pg = st.navigation([st.Page("uxvault/intro.py",icon=':material/home:' ,title="Home"),
-                    st.Page("uxvault/about.py",icon=':material/info:' ,title="About Us"),
-                    st.Page("uxvault/create_card_sorting.py",icon=':material/create:' ,title="Create Card Sorting"),
-                    st.Page("uxvault/solve_card_sorting.py",icon=':material/checklist_rtl:' ,title="Solve Card Sorting"),
-                    st.Page("uxvault/dashboard.py",icon=':material/dashboard:' ,title="Dashboard")
-                   ],
+pg = st.navigation({
+    "Who we are": [st.Page("uxvault/intro.py",icon=':material/home:' ,title="Home") , st.Page("uxvault/about.py",icon=':material/info:' ,title="About Us")],
+    "Tools": [st.Page("uxvault/create_card_sorting.py",icon=':material/create:' ,title="Create Card Sorting"),
+              st.Page("uxvault/solve_card_sorting.py",icon=':material/checklist_rtl:' ,title="Solve Card Sorting"),
+              st.Page("uxvault/dashboard.py",icon=':material/dashboard:' ,title="Dashboard")]
+},
 position='top')
 
 pg.run()
